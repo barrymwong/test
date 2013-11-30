38cd65c9e0f4c378336dba4b9d9374fafbeb9ee9
Barry Wong <barry@barrymwong.com>
2013-11-30 10:04:33 -0800

    add grunt notify

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -1,47 +1,54 @@
 module.exports = function(grunt) {
-	grunt.loadNpmTasks("grunt-contrib-sass");
-	grunt.loadNpmTasks("grunt-contrib-concat");
-	grunt.loadNpmTasks("grunt-contrib-uglify");
-	grunt.loadNpmTasks("grunt-contrib-watch");
-
 	grunt.initConfig({
 		sass: {
 			dist: {
 				options: {
-					style: "expanded"
+					style: 'expanded'
 				},
 				files: [{
-					"css/styles/style1.css": "css/sass/style1.scss",
-					"css/styles/style2.css": "css/sass/style2.scss"
+					'css/styles/style1.css': 'css/sass/style1.scss',
+					'css/styles/style2.css': 'css/sass/style2.scss'
 				}]
 			}
 		},
 
 		concat: {
 			js: {
-				src: "js/scripts/*.js",
-				dest: "js/app.js"
+				src: 'js/scripts/*.js',
+				dest: 'js/app.js'
 			},
 			css: {
-				src: "css/styles/*.css",
-				dest: "css/app.css"
+				src: 'css/styles/*.css',
+				dest: 'css/app.css'
 			}
 		},
 
 		watch: {
 			options: {
 				livereload: true,
+				title: 'Watch',
+				message: 'Watch has completed'
 			},
 			js: {
-				files: ["js/scripts/*.js"],
-				tasks: ["concat:js"]
+				files: ['js/scripts/*.js'],
+				tasks: ['concat:js']
 			},
 			css: {
-				files: ["css/**/*.scss"],
-				tasks: ["sass", "concat:css"]
+				files: ['css/**/*.scss'],
+				tasks: ['sass', 'concat:css']
 			}
 		}
 	});
 
-	grunt.registerTask("dev", ["sass", "concat", "watch"]);
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-notify');
+
+	grunt.registerTask('dev', [
+		'sass',
+		'concat',
+		'watch'
+	]);
 };
