d12fca46640b958be93da9ee63718bc6b7bd8fe6
Barry Wong <barry@barrymwong.com>
2013-11-30 11:40:16 -0800

    remove uglify

--- grunt/Gruntfile.js
+++ grunt/Gruntfile.js
@@ -42,7 +42,6 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-notify');
 
